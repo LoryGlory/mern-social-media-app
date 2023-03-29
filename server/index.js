7602a261d1294afdee77451ce9c01acd25538f72
Laura Roganovic <laura.roganovic@googlemail.com>
2023-03-29 19:09:09 +0200

    add register route and import for controller

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import {fileURLToPath} from 'url';
+import {register} from './controllers/auth.js';
 
 // middleware config
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+
+// routes with files
+app.post('/auth/register', upload.single('picture,'), register);
 
 // mongoose setup
 const PORT = process.env.PORT || 6001;
