283991d36ddf6da3962b56ba963813d8ac46c87c
Laura Roganovic <laura.roganovic@googlemail.com>
2023-04-12 18:25:42 +0200

    fix bugs with userId

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,11 +1,11 @@
-import User from '../models/User.js';
 import Post from '../models/Post.js';
+import User from '../models/User.js';
 
-// create
+/* CREATE */
 export const createPost = async (req, res) => {
   try {
     const {userId, description, picturePath} = req.body;
-    const user = await User.findById;
+    const user = await User.findById(userId);
     const newPost = new Post({
       userId,
       firstName: user.firstName,
@@ -17,19 +17,16 @@ export const createPost = async (req, res) => {
       likes: {},
       comments: [],
     });
-
     await newPost.save();
-    const post = await Post.find();
 
+    const post = await Post.find();
     res.status(201).json(post);
   } catch (err) {
-    res.status(409).json({
-      message: err.message,
-    });
+    res.status(409).json({message: err.message});
   }
 };
 
-// read
+/* READ */
 export const getFeedPosts = async (req, res) => {
   try {
     const post = await Post.find();
@@ -49,7 +46,7 @@ export const getUserPosts = async (req, res) => {
   }
 };
 
-// update
+/* UPDATE */
 export const likePost = async (req, res) => {
   try {
     const {id} = req.params;
@@ -62,13 +59,14 @@ export const likePost = async (req, res) => {
     } else {
       post.likes.set(userId, true);
     }
+
     const updatedPost = await Post.findByIdAndUpdate(
       id,
       {likes: post.likes},
       {new: true},
     );
 
-    res.status(200).json();
+    res.status(200).json(updatedPost);
   } catch (err) {
     res.status(404).json({message: err.message});
   }
