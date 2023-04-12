7352dac56784480b67d3ead139c4859c58f80395
Laura Roganovic <laura.roganovic@googlemail.com>
2023-04-12 18:26:21 +0200

    add button text

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -17,10 +17,10 @@ import {
   IconButton,
   useMediaQuery,
 } from '@mui/material';
-import FlexBetween from '../../components/FlexBetween';
+import FlexBetween from 'components/FlexBetween';
 import Dropzone from 'react-dropzone';
-import UserImage from '../../components/UserImage';
-import WidgetWrapper from '../../components/WidgetWrapper';
+import UserImage from 'components/UserImage';
+import WidgetWrapper from 'components/WidgetWrapper';
 import {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {setPosts} from 'state';
@@ -33,7 +33,7 @@ const MyPostWidget = ({picturePath}) => {
   const {palette} = useTheme();
   const {_id} = useSelector((state) => state.user);
   const token = useSelector((state) => state.token);
-  const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
   const mediumMain = palette.neutral.mediumMain;
   const medium = palette.neutral.medium;
 
@@ -41,19 +41,18 @@ const MyPostWidget = ({picturePath}) => {
     const formData = new FormData();
     formData.append('userId', _id);
     formData.append('description', post);
-
     if (image) {
       formData.append('picture', image);
       formData.append('picturePath', image.name);
     }
 
-    const response = await fetch(`http://localhost:3001/posts`,
-      {
-        method: 'POST',
-        headers: {Authorization: `Bearer ${token}`},
-        body: formData,
-      });
+    const response = await fetch(`http://localhost:3001/posts`, {
+      method: 'POST',
+      headers: {Authorization: `Bearer ${token}`},
+      body: formData,
+    });
     const posts = await response.json();
+    dispatch(setPosts({posts}));
     setImage(null);
     setPost('');
   };
@@ -63,8 +62,8 @@ const MyPostWidget = ({picturePath}) => {
       <FlexBetween gap='1.5rem'>
         <UserImage image={picturePath} />
         <InputBase
-          placeholder="'What's on your mind...?"
-          onChange={(e) => setPosts(e.target.value)}
+          placeholder="What's on your mind..."
+          onChange={(e) => setPost(e.target.value)}
           value={post}
           sx={{
             width: '100%',
@@ -76,17 +75,15 @@ const MyPostWidget = ({picturePath}) => {
       </FlexBetween>
       {isImage && (
         <Box
-          borderRadius='5px'
           border={`1px solid ${medium}`}
+          borderRadius='5px'
           mt='1rem'
           p='1rem'
         >
           <Dropzone
             acceptedFiles='.jpg,.jpeg,.png'
             multiple={false}
-            onDrop={acceptedFiles =>
-              setImage(acceptedFiles[0])
-            }
+            onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
           >
             {({getRootProps, getInputProps}) => (
               <FlexBetween>
@@ -95,15 +92,14 @@ const MyPostWidget = ({picturePath}) => {
                   border={`2px dashed ${palette.primary.main}`}
                   p='1rem'
                   width='100%'
-                  sx={{':&hover': {cursor: 'pointer'}}}
+                  sx={{'&:hover': {cursor: 'pointer'}}}
                 >
-                  < input {...getInputProps()} />
+                  <input {...getInputProps()} />
                   {!image ? (
                     <p>Add Image Here</p>
-                  ) : (<FlexBetween>
-                      <Typography>
-                        {image.name}
-                      </Typography>
+                  ) : (
+                    <FlexBetween>
+                      <Typography>{image.name}</Typography>
                       <EditOutlined />
                     </FlexBetween>
                   )}
@@ -132,27 +128,28 @@ const MyPostWidget = ({picturePath}) => {
             sx={{'&:hover': {cursor: 'pointer', color: medium}}}
           >
             Image
-          < /Typography>
+          </Typography>
         </FlexBetween>
+
         {isNonMobileScreens ? (
           <>
             <FlexBetween gap='0.25rem'>
               <GifBoxOutlined sx={{color: mediumMain}} />
-              <Typography color={mediumMain}> Clip</Typography>
+              <Typography color={mediumMain}>Clip</Typography>
             </FlexBetween>
 
             <FlexBetween gap='0.25rem'>
               <AttachFileOutlined sx={{color: mediumMain}} />
-              <Typography color={mediumMain}> Attachment</Typography>
+              <Typography color={mediumMain}>Attachment</Typography>
             </FlexBetween>
 
             <FlexBetween gap='0.25rem'>
               <MicOutlined sx={{color: mediumMain}} />
-              <Typography color={mediumMain}> Audio</Typography>
+              <Typography color={mediumMain}>Audio</Typography>
             </FlexBetween>
           </>
         ) : (
-          <FlexBetween>
+          <FlexBetween gap='0.25rem'>
             <MoreHorizOutlined sx={{color: mediumMain}} />
           </FlexBetween>
         )}
@@ -166,6 +163,7 @@ const MyPostWidget = ({picturePath}) => {
             borderRadius: '3rem',
           }}
         >
+          POST
         </Button>
       </FlexBetween>
     </WidgetWrapper>
