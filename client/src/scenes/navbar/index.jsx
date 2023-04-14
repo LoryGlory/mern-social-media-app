b05ef2d657180fe0bb0a9aae303d203f3ab79c81
Laura Roganovic <laura.roganovic@googlemail.com>
2023-04-14 14:15:04 +0200

    renamed to hivemind

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Hivemind
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
