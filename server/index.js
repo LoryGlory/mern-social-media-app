ea9c27fffc01141c831fbaa3b75063e3d3756776
Laura Roganovic <laura.roganovic@googlemail.com>
2023-04-15 21:49:40 +0200

    add client/build folder to express

--- server/index.js
+++ server/index.js
@@ -32,6 +32,8 @@ app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+//test
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
